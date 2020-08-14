58d1a302675bb3c1aa60f440f3546ff059ef92ca
Prabhakaran Jayaraman Masani <prabhakaran.jm@gmail.com>
2020-08-14 23:27:45 +0530

    Completed module4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,3 +1,18 @@
 import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+  constuctor(props) {
+    super(props)
+    this.state = { quiz_position: 1 }
+  }
+  render() {
+    return (
+      <div><div className="QuizQuestion">{quizData.quiz_questions[0]
+      .instruction_text}</div>
+    )
+  }
+}
+
+export default Quiz
