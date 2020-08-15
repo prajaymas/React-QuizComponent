cae92920d68429d2254166128f93f6ccf188078e
Prabhakaran Jayaraman Masani <prabhakaran.jm@gmail.com>
2020-08-15 15:32:42 +0530

    Updated Code

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,12 +1,1 @@
 import React, { Component } from 'react'
-
-class QuizQuestionButton extends Component {
-    render() {
-        <li><button></li>
-        <div>
-        <button_text={[this.props.quiz_question.answer_options[0]]}/>
-      </div> 
-    }
-}
-
-export default QuizQuestionButton
